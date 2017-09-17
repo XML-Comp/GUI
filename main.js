e6d230acbbe65a1016d8726893fef54937df1cbd
Arthur Silva <arxdsilva@gmail.com>
2017-09-17 04:14:59 -0300

    solves mc

---
+++ main.js
@@ -0,0 +1,32 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 450, height: 300, resizable: false})
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', function () {mainWindow = null})
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {if (mainWindow === null) {createWindow()}})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
